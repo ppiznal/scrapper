ae13bf3449d8b2ea09041154870a2454105985d2
Przemek <ppiznal@gmail.com>
2019-08-03 13:48:36 +0200

    Script works

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # scrapper
 Pyhton web scrapping
+
+
+
+# requirments
+pip install selenium
+or pip3 install selenium
+
+installed firefox
+
+downloaded geckodriver from https://github.com/mozilla/geckodriver/releases/latest
---
+++ selenium_scrapping.py
@@ -0,0 +1,15 @@
+# #!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+capabilities = webdriver.DesiredCapabilities().FIREFOX
+capabilities["marionette"] = True
+
+# path to firefox
+binary = FirefoxBinary('/opt/firefox/firefox')
+# path to geckodriver
+browser = webdriver.Firefox(capabilities=capabilities, firefox_binary=binary, executable_path='../geckodriver')
+browser.get('https://google.com')
+browser.quit()
